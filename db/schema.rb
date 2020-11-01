0f4a9c879b6ca27a53e1dd77c32d4ad4800e0604
behraaangm <behraaangm@gmail.com>
2020-11-01 19:21:21 +0330

    add pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_31_124609) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
