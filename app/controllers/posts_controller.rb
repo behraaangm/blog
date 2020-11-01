cfc2163579d4d1f7b454955b7d17776771575a07
behraaangm <behraaangm@gmail.com>
2020-11-01 19:48:47 +0330

    create and update for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,36 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save!
+      redirect_to action: :index
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = 'Post edited successfully'
+      redirect_to action: :index
+    else
+      flash[:alert] = @post.errors.full_messages
+      redirect_to action: :edit, id: params[:id]
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :tag_ids)
+  end
 end
